392862de8876aebaf7695593a8bdb7c3217638fc
kazuyan123 <kazu.h9898@gmail.com>
2024-11-21 04:23:23 +0900

    Update suikagame.py

--- suikagame.py
+++ suikagame.py
@@ -3,14 +3,14 @@ import pyxel
 class App:
     def __init__(self): # 初期化
         pyxel.init(160, 256)
-        self.x = 0
-        pyxel.run(self.update, self.draw) # アプリケーションの実行
+        # self.x = 0
+        # pyxel.run(self.update, self.draw) # アプリケーションの実行
 
-    def update(self): # フレームの更新処理
-        self.x = (self.x + 1) % pyxel.width
+    # def update(self): # フレームの更新処理
+    #     self.x = (self.x + 1) % pyxel.width
 
-    def draw(self): # 描画処理
-        pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
+    # def draw(self): # 描画処理
+    #     pyxel.cls(0)
+    #     pyxel.rect(self.x, 0, 8, 8, 9)
 
 App()
