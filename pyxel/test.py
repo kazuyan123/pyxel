9d7d6844b8e2726f4b2d1085fdaea8aa731ebd37
kazuyan123 <kazu.h9898@gmail.com>
2024-11-21 18:13:28 +0900

    Update test.py

--- pyxel/test.py
+++ pyxel/test.py
@@ -1,1 +1,16 @@
+import pyxel
 
+class App:
+    def __init__(self): # 初期化
+        pyxel.init(100, 100)
+        self.x = 0
+        pyxel.run(self.update, self.draw) # アプリケーションの実行
+
+    def update(self): # フレームの更新処理
+        self.x = (self.x + 1) % pyxel.width
+
+    def draw(self): # 描画処理
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9)
+
+App()
