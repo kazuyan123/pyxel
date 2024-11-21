d632e9b9f430e603a0ec5a6977d5bb4b7568db33
Kazunori Hashimoto <kazu.h9898@gmail.com>
2024-11-21 19:13:09 +0900

    Update test.py

--- pyxel/test.py
+++ pyxel/test.py
@@ -2,7 +2,7 @@ import pyxel
 
 class App:
     def __init__(self): # 初期化
-        pyxel.init(100, 100, gamepad = False)
+        pyxel.init(160, 256, gamepad = False)
         self.x = 0
         pyxel.run(self.update, self.draw) # アプリケーションの実行
 
