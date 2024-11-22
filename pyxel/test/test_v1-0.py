538b08f2741e4c0cf3606e9b2994f2f6748a315e
Kazunori Hashimoto <kazu.h9898@gmail.com>
2024-11-22 16:00:29 +0900

    Update and rename test_v1-0 to test_v1-0.py

---
+++ pyxel/test/test_v1-0.py
@@ -0,0 +1,17 @@
+import pyxel
+
+class App:
+    def __init__(self): # 初期化
+        pyxel.init(160, 256, title="test") 
+        self.x = 0
+        pyxel.show()
+        # pyxel.run(self.update, self.draw) # アプリケーションの実行
+
+    def update(self): # フレームの更新処理
+        self.x = (self.x + 1) % pyxel.width
+
+    def draw(self): # 描画処理
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9)
+
+App()
